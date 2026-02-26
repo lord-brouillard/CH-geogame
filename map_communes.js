31870236c240e63f96a84c63f4fd111fdb007345
lord-brouillard <matthias.renaud@bluewin.ch>
2026-02-26 13:56:22 +0100

    6th commit

--- map_communes.js
+++ map_communes.js
@@ -20,7 +20,7 @@ fetch('./data/GeoJSON_communes.geojson')
 
                 allFeatures.push(lyr);
 
-                const html = `${feature.properties.NAME}`;
+                const html = ` <strong>${props.NAME}</strong><br> Surface : ${props.AREA ?? "?"} km²<br> Population : ${props.POPULATION ?? "?"} habitants `;
 
                 lyr.bindPopup(html);
 
