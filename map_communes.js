a6cf31f8b8b3b665e7d8ebfa8692c5a94dad6a05
lord-brouillard <matthias.renaud@bluewin.ch>
2026-02-26 14:17:11 +0100

    Update map_communes.js

--- map_communes.js
+++ map_communes.js
@@ -1,25 +1,79 @@
+let allFeatures = []; // stockage des features
+
+const map = L.map('map', {
+    zoomControl: false,
+    attributionControl: false
+});
+
+// Contrôle zoom
+L.control.zoom({
+    position: 'topright'
+}).addTo(map);
+
+// Chargement du GeoJSON
 fetch('./data/GeoJSON_communes.geojson')
     .then(response => response.json())
     .then(geojson => {
 
         const layer = L.geoJSON(geojson, {
-            ...
+            style: {
+                color: '#000',
+                weight: 1,
+                fillOpacity: 0.01
+            },
+
             onEachFeature: (feature, lyr) => {
+
                 allFeatures.push(lyr);
-                ...
+
+                const p = feature.properties;
+
+                // Contenu affiché dans popup + panneau du bas
+                const html = `
+                    <b>${p.NAME}</b><br>
+                    Population : ${p.EINWOHNERZ.toLocaleString('fr-CH')} habitants<br>
+                    Surface : ${p.GEM_FLAECH} ha
+                `;
+
+                // Popup
+                lyr.bindPopup(html);
+
+                // Affichage dans le panneau du bas
+                lyr.on('click', () => {
+                    document.getElementById('info').innerHTML = html;
+                });
+
+                // Effet hover
+                lyr.on('mouseover', () => {
+                    lyr.setStyle({ weight: 3, color: 'blue' });
+                });
+
+                lyr.on('mouseout', () => {
+                    lyr.setStyle({ weight: 1, color: '#000' });
+                });
             }
         });
 
+        // Ajout à la carte
         layer.addTo(map);
+
+        // Zoom automatique
         map.fitBounds(layer.getBounds());
 
         // --- Recherche ---
         document.getElementById('btnSearch').addEventListener('click', () => {
             const q = document.getElementById('search').value.toLowerCase();
+
             const found = allFeatures.filter(f =>
                 f.feature.properties.NAME.toLowerCase().includes(q)
             );
-            ...
+
+            if (found.length > 0) {
+                map.fitBounds(found[0].getBounds());
+                found[0].openPopup();
+            } else {
+                alert("Commune non trouvée");
+            }
         });
 
         // --- Autocomplétion ---
@@ -43,6 +97,7 @@ fetch('./data/GeoJSON_communes.geojson')
                 div.addEventListener('click', () => {
                     input.value = f.feature.properties.NAME;
                     suggestions.innerHTML = '';
+
                     map.fitBounds(f.getBounds());
                     f.openPopup();
                 });
@@ -50,4 +105,5 @@ fetch('./data/GeoJSON_communes.geojson')
                 suggestions.appendChild(div);
             });
         });
+
     });
